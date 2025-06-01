5bffc8def8817565c9e6ab7cff13cb009edec958
VladTca <votsats@gmail.com>
2025-06-01 06:49:07 +0200

    feat(build): enable rollup visualizer and optimize build configuration
    Добавлен плагин `rollup-plugin-visualizer` для анализа сборки и открытия отчета в браузере 📊.
    Обновлена конфигурация сборки Vite с добавлением параметра `manualChunks` для разбиения крупных библиотек (например, React) на отдельные чанки, что улучшает производительность 🛠.
    Обновлена конфигурация ESLint: добавлена поддержка правил `react` через `eslint-plugin-react` и настройка версии React автоматически через `react version: detect` ✨.
    Расширены зависимости в `pnpm-lock.yaml` и `package.json` для поддержки новых плагинов и библиотек.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {visualizer} from "rollup-plugin-visualizer";
 
 export default defineConfig({
   base: '/vitpnpm/',
@@ -9,5 +10,22 @@ export default defineConfig({
         plugins: ['babel-plugin-styled-components'],
       },
     }),
+    visualizer({ open: true }) // Открыть анализ в браузере
+
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        // Разбиваем крупные библиотеки на отдельные чанки
+        manualChunks: {
+          react: ['react', 'react-dom'], // Вынос React в отдельный чанк
+          vendor: [
+            'styled-components',
+            'react-awesome-reveal',
+            // Укажите крупные библиотеки
+          ],
+        },
+      },
+    },
+  },
 });
